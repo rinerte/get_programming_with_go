9c5bf6e675bb89c84143fdf8d6f9343d2d097fab
rinerte <rinerte@gmail.com>
2024-12-23 11:14:19 +0900

    weight and age on Mars

---
+++ hello_mars/mars.go
@@ -0,0 +1,12 @@
+//comment
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Print("My weight on Mars is ")
+	fmt.Print(80.0*0.3783)
+	fmt.Print(" kg. And I would be ")
+	fmt.Print(34*365 / 687)
+	fmt.Print(" years old.")
+}
