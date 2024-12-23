23c88a1d87b6bc956cfff18ebee482ad4dfbed51
rinerte <rinerte@gmail.com>
2024-12-23 13:38:52 +0900

    lesson 2 summary

--- hello_mars/mars.go
+++ hello_mars/mars.go
@@ -4,11 +4,14 @@ package main
 import "fmt"
 
 func main(){
-	const lightSpeed = 299792
-	var speed = 100800 
-	var distance = 96300000
+	// const lightSpeed = 299792
+	// var speed = 100800 
+	// var distance = 96300000
 
-	fmt.Println(distance/lightSpeed)
+	// fmt.Println(distance/lightSpeed)
 
-	fmt.Println(distance/speed/24)
+	// fmt.Println(distance/speed/24)
+	
+	// WHAT speed we need to reach Malacandra in 28 days?
+	fmt.Println("Answer= ",56000000/(28*24))
 }
