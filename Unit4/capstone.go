61624d33749f1d050ad6175bea78ad70d20feb13
rinerte <rinerte@gmail.com>
2025-02-06 10:12:41 +0900

    ok

--- Unit4/capstone.go
+++ Unit4/capstone.go
@@ -8,8 +8,8 @@ import (
     "os/exec"
 )
 const (
-	width = 80
-	height = 15
+	width = 120
+	height = 30
 	alive = '*'
 	dead = ' '
 )
@@ -23,19 +23,6 @@ func NewUniverse() Universe {
 	return u;
 }
 
-// func (u Universe) Show(){
-// 	for i:=0;i<height; i++ {
-// 		for j:=0;j<width;j++{
-// 			if (u[i][j]){
-// 				fmt.Printf("%c",alive)
-// 			} else {
-// 				fmt.Printf("%c",dead)
-// 			}
-			
-// 		}
-// 		fmt.Print("|\n")
-// 	}
-// }
 func (u Universe) Show() {
 	cmd := exec.Command("clear") //Linux example, its tested
 	cmd.Stdout = os.Stdout
@@ -47,9 +34,9 @@ func (u Universe) String() string {
 	buf := make([]byte, 0, (width+1)*height)
 	for y := 0; y < height; y++ {
 		for x := 0; x < width; x++ {
-			b = ' '
+			b = dead
 			if u[y][x] {
-				b = '*'
+				b = alive
 			}
 			buf = append(buf, b)
 		}
@@ -71,26 +58,18 @@ func (u Universe) Seed(){
 	}
 }
 func (u Universe) Alive(i,j int) bool{
-	if(i==-1){
-		i = height - 1
-	}
-	if(j==-1){
-		j = width - 1
-	}
-	if(i==height){
-		i = 0
-	}
-	if(j==width){
-		j = 0
-	}
-	return u[i][j]
+	
+	x:= (j + width) % width
+	y:= (i + height) % height
+	return u[y][x]
 }
 
 func (u Universe) Neighbours(i,j int) int{
-	count:=0
-	for row:=i-1; row<i+2; row++{
-		for col:=j-1;col<j+2;col++{
-			if((col!=j || row!=i)&&u.Alive(i,j)){
+	
+	count := 0
+	for row := i - 1; row < i+2; row++ {
+		for col := j - 1; col < j+2; col++ {
+			if (col != j || row != i) && u.Alive(row, col) {
 				count++
 			}
 		}
@@ -99,19 +78,8 @@ func (u Universe) Neighbours(i,j int) int{
 }
 
 func (u Universe) Next (j,i int) bool {
-	count:=u.Neighbours(i,j)
-	alive:=u.Alive(i,j)
-
-	if(alive){
-		if(count!=2 && count!=3) {
-			return false
-		}
-	} else{
-		if (count==3){
-			return true;
-		}
-	}
-	return alive
+	n:=u.Neighbours(i,j)
+	return n == 3 || n == 2 && u.Alive(i, j)
 }
 
 func Step(a,b Universe){
@@ -132,7 +100,7 @@ func main(){
 	for i := 0; i < 300; i++ {
 		Step(a, b)
 		a.Show()
-		time.Sleep(time.Second)
+		time.Sleep(time.Second/24)
 		a, b = b, a 
 	}	
 }
