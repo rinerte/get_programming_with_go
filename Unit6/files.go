56dfa9c1b405e92b9b47ac3a15e50c065bd568a5
rinerte <rinerte@gmail.com>
2025-02-28 09:17:00 +0900

    files root

--- Unit6/files.go
+++ Unit6/files.go
@@ -4,15 +4,50 @@ import (
 	"fmt"
 	"os"
 	"io/ioutil"
+
+    "path/filepath"
 )
 
 func main() {
-	files, err := ioutil.ReadDir(".")
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	for _, file := range files {
-		fmt.Println(file.Name())
-	}
+	// files, err := ioutil.ReadDir("../.")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	os.Exit(1)
+	// }
+	// for _, file := range files {
+	// 	fmt.Println(file.Name())
+	// }
+
+	currentPath, err := os.Getwd()
+    if err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
+
+    // Пытаемся подняться до корня
+    var lastValidPath string
+    for {
+        _, err = ioutil.ReadDir(currentPath)
+        if err != nil {
+            break
+        }
+        
+        lastValidPath = currentPath
+        currentPath = 	filepath.Dir(currentPath) 
+        
+        if currentPath == filepath.Dir(currentPath) {
+            break
+        }
+    }
+
+    files, err := ioutil.ReadDir(lastValidPath)
+    if err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
+    
+    fmt.Println("Root directory:", lastValidPath)
+    for _, file := range files {
+        fmt.Println(file.Name())
+    }
 }
