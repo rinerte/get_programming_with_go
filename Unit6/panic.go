aac918636d4c99dd82354163616dc89877518ebb
rinerte <rinerte@gmail.com>
2025-03-01 16:08:36 +0900

    panic

---
+++ Unit6/panic.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main(){
+	// defer func() {
+	// 	if e := recover(); e != nil {
+	// 	fmt.Println(e)
+	// 	}
+	// }()
+	defer heal()
+	panic("Message!")
+}
+
+func heal() {
+	if e := recover(); e != nil {
+		fmt.Println(e)
+	}
+}
