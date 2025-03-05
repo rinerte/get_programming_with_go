321e1240c0058da7c109736990c259b70a83c29a
rinerte <rinerte@gmail.com>
2025-03-05 14:05:02 +0900

    chan

--- Unit7/sleepygophers.go
+++ Unit7/sleepygophers.go
@@ -6,12 +6,19 @@ import (
 )
 
 func main() {
+	c:=make(chan int)
+
 	for i := 0; i < 5; i++ {
-		go sleepyGopher(i)
+		go sleepyGopher(i,c)
 	}
-	time.Sleep(4 * time.Second)
+	for i := 0; i < 5; i++ {
+		r:= <-c
+		fmt.Println("gopher with id ",r," finished sleeping")
+	}
 }
-func sleepyGopher(i int) {
+	
+func sleepyGopher(i int,c chan int) {
 	time.Sleep(3 * time.Second)
 	fmt.Println("... snore ...",i)
+	c <-i
 }
