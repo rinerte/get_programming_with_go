eb26aa5e1d6b5db413bcee6b85e20561111ce9ac
rinerte <rinerte@gmail.com>
2025-03-10 16:02:22 +0900

    race condition prevented with mutex

--- Unit7/mutex.go
+++ Unit7/mutex.go
@@ -2,21 +2,25 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 func main() {
 	i := 0
+	var mutex sync.Mutex
 
-	go inc(&i)
-	go inc(&i)
-	go inc(&i)
+	go inc(&i, &mutex)
+	go inc(&i, &mutex)
+	go inc(&i, &mutex)
 	time.Sleep(3 * time.Second)
 	fmt.Println(i)
 }
 
-func inc(n *int) {
-	for i := 0; i < 100000000; i++ {
+func inc(n *int, mutex *sync.Mutex) {
+	for i := 0; i < 10000000; i++ {
+		mutex.Lock()
 		*n++
+		mutex.Unlock()
 	}
 }
